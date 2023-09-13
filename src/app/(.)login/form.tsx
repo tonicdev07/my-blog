155f31abaacce1bb38687450e7a5e09922be268a
Anvarbek <anvarbekxaydarov777@gmail.com>
2023-09-13 14:39:19 +0500

    update12

--- src/app/(.)login/form.tsx
+++ src/app/(.)login/form.tsx
@@ -123,20 +123,6 @@ export const LoginForm = () => {
               
                 Google orqali kirish
               </a>
-              {/* <a
-                className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
-                style={{ backgroundColor: "#55acee" }}
-                onClick={() => signIn("facebook")}
-                role="button"
-              >
-                <img
-                  className="pr-2"
-                  src="/images/github.svg"
-                  alt=""
-                  style={{ height: "2.2rem" }}
-                />
-                Continue with Facebook
-              </a> */}
               <div>
                 <span>Account yo'qmi? </span>
                 <Link className=" text-sm text-blue-600" href={"/signup"}>
